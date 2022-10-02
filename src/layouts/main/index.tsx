fa77b25f4ff5a36b8e41279d531002bd37532657
Wildan Fadh <wildanfadh79@gmail.com>
2022-10-03 06:35:54 +0700

    update banner homepage

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -1,10 +1,90 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export const MainLayout = () => {
   return (
     <div>
-      MainLayout <Outlet />
+      <div className="navbar bg-base-100 px-5 fixed bg-transparent z-10 lg:px-24">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost btn-circle lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Shop</a>
+              </li>
+              <li>
+                <a>Order</a>
+              </li>
+              <li>
+                <a>Cart</a>
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-sm btn-ghost normal-case text-xl">EasyShop</a>
+        </div>
+        <div className="navbar-end lg:hidden">
+          <div className="flex-row space-x-3">
+            <NavLink to="/auth/register" className="btn btn-sm btn-neutral">
+              Sign Up
+            </NavLink>
+            <NavLink
+              to="/auth/login"
+              className="btn btn-sm btn-accent border-inherit"
+            >
+              Login
+            </NavLink>
+          </div>
+        </div>
+        <div className="navbar-end hidden lg:flex">
+          <div className="flex-row space-x-3">
+            <ul className="menu menu-horizontal menu-compact p-0">
+              <li>
+                <a>Shop</a>
+              </li>
+              <li>
+                <a>Order</a>
+              </li>
+              <li>
+                <a>Cart</a>
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
+            </ul>
+            <NavLink to="/auth/register" className="btn btn-sm btn-neutral">
+              Sign Up
+            </NavLink>
+            <NavLink
+              to="/auth/login"
+              className="btn btn-sm btn-accent border-inherit"
+            >
+              Login
+            </NavLink>
+          </div>
+        </div>
+      </div>
+      <Outlet />
     </div>
   );
 };
--- src/views/main/home/index.tsx
+++ src/views/main/home/index.tsx
@@ -1,5 +1,30 @@
 import React from "react";
 
+// assets
+import heroVector from "../../../assets/vector1.svg";
+import Image1 from "../../../assets/Image1.png";
+import Image2 from "../../../assets/Image2.png";
+
 export const HomeMainViews = () => {
-  return <div>HomeMainViews</div>;
+  return (
+    <div>
+      <div className="flex w-full h-[700px] bg-slate-100">
+        <img
+          src={heroVector}
+          alt="vector1"
+          className="absolute top-[-40px] lg:top-0 right-0 w-[620px] h-[800px] z-0"
+        />
+        <img
+          src={Image1}
+          alt="image 1"
+          className="hidden lg:block absolute right-0 top-[90px] w-5/12"
+        />
+        <img
+          src={Image2}
+          alt="image 2"
+          className="hidden lg:block absolute right-[15%] top-[60px] w-6/12"
+        />
+      </div>
+    </div>
+  );
 };
